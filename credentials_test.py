a6bf527df81d6d16a9a54ebb54448abdcdf2554f
Tajeu2001 <tajeusanta@gmail.com>
2021-05-30 01:49:23 +0300

    Add a setUp method

--- credentials_test.py
+++ credentials_test.py
@@ -3,15 +3,15 @@ from credentials import Credential
 
 class TestCredential(unittest.TestCase):
     '''
-#     Test class that defines test cases for the credential class behaviour.
-#     Args:
-#         unittest.TestCase
-#     '''
-#     def setUp(self):
-#       '''
-#       Set up method to run before each test case
-#       ''' 
-#       self.new_credential = Credential("Twitter", "Tajeu2001", "siantayo15")
+    Test class that defines test cases for the credential class behaviour.
+    Args:
+        unittest.TestCase
+    '''
+    def setUp(self):
+      '''
+      Set up method to run before each test case
+      ''' 
+      self.new_credential = Credential("Twitter", "Tajeu2001", "siantayo15")
 
 #     def test_init(self):
 #       '''
