d7d2cb1f89f3373ef5981381c909d620221cfea2
Tajeu2001 <tajeusanta@gmail.com>
2021-05-30 01:51:42 +0300

    Test the new credential class instance

--- credentials_test.py
+++ credentials_test.py
@@ -14,11 +14,11 @@ class TestCredential(unittest.TestCase):
       self.new_credential = Credential("Twitter", "Tajeu2001", "siantayo15")
 
     def test_init(self):
-#       '''
-#       test_init test case to test if the object is initialized properly 
-#       '''  
+      '''
+      test_init test case to test if the object is initialized properly 
+      '''  
 
-#       self.assertEqual(self.new_credential.account , "Twitter")  
+      self.assertEqual(self.new_credential.account , "Twitter")  
 #       self.assertEqual(self.new_credential.username , "Tajeu2001")
 #       self.assertEqual(self.new_credential.password , "siantayo15")
 
