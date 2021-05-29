12964cf8dc5ebbf7c8ebe9cbed10dffc1e7e566f
Tajeu2001 <tajeusanta@gmail.com>
2021-05-30 01:16:49 +0300

    Create a user.py file

---
+++ user.py
@@ -0,0 +1,27 @@
+'''
+User class for the password locker account
+'''
+
+# class User:
+
+#     '''
+#     class used as a blueprint to generate other user accounts
+#     '''
+
+#     user_list = []
+
+#     def __init__ (self,username,password):
+
+#       '''
+#       method to define properties for the user class objec
+#       '''
+    
+#       self.username = username
+#       self.password = password
+
+#     def save_user(self):
+#       '''
+#       method that adds a new user to the user list
+#       '''
+
+#       User.user_list.append(self)
