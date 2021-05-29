51cc08aafcf7618f45a11c3987b9e70848bb30a8
Tajeu2001 <tajeusanta@gmail.com>
2021-05-30 01:20:26 +0300

    Create a user list object

--- user.py
+++ user.py
@@ -4,11 +4,11 @@ User class for the password locker account
 
 class User:
 
-#     '''
-#     class used as a blueprint to generate other user accounts
-#     '''
+    '''
+    class used as a blueprint to generate other user accounts
+    '''
 
-#     user_list = []
+    user_list = []
 
 #     def __init__ (self,username,password):
 
