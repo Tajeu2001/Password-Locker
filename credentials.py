816dbfea38dec0927aff602e30b03c4655ebcc46
Tajeu2001 <tajeusanta@gmail.com>
2021-05-30 01:36:46 +0300

    create the credential.py file

---
+++ credentials.py
@@ -0,0 +1,27 @@
+'''
+Credentials class for creating user credentials
+'''
+
+# class Credential:
+
+#     '''
+#     class used as a blueprint to generate other instances of user credentials
+#     '''
+
+#     credential_list = []
+
+#     def __init__(self,account,username,password):
+#       '''
+#       method to define properties for the credential class object
+#       '''
+      
+#       self.account = account
+#       self.username = username
+#       self.password = password
+
+#     def save_credential(self):
+#       '''
+#       method that adds a new user to the user list
+#       '''
+
+#       Credential.credential_list.append(self)
