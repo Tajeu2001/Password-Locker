9b8c321c2e4224895ade60ad401304aacccf396c
Tajeu2001 <tajeusanta@gmail.com>
2021-05-31 01:51:38 +0300

    Add the pycache folder

--- credentials_test.py
+++ credentials_test.py
@@ -19,8 +19,33 @@ class TestCredential(unittest.TestCase):
       '''  
 
       self.assertEqual(self.new_credential.account , "Twitter")  
-      self.assertEqual(self.new_credential.username , "Tajeu2001")
-      self.assertEqual(self.new_credential.password , "siantayo15")
+      self.assertEqual(self.new_credential.cred_username , "Tajeu2001")
+      self.assertEqual(self.new_credential.cred_password , "siantayo15")
+
+
+    def test_delete_credential(self):
+      '''
+      test if credentials can be deleted
+      '''
+      self.new_credential.save_credential()
+      test_credential = Credential("Instagram", "Tajeu2001", "siantayo15") 
+      test_credential.save_credential()
+      self.new_credential.delete_credential()
+      self.assertEqual(len(Credential.credential_list), 1) 
+
+
+    def test_generate_password(self):
+      '''
+      Test to see if a random password can be generated
+      '''
+      password = self.new_credential.generate_password()
+      self.assertEqual(len(password), 6)
+
+    def test_view_credentials(self):
+      '''
+      test if credentials can be viewed
+      '''
+      self.assertEqual(Credential.view_credentials(),Credential.credential_list)  
 
 
 if __name__ == '__main__':
