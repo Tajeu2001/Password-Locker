f580e87c3631c4335d51170c040e3ccd57b50b5f
Tajeu2001 <tajeusanta@gmail.com>
2021-05-31 01:22:34 +0300

    Allow the user to create new credentials

--- run.py
+++ run.py
@@ -65,50 +65,28 @@ def main():
             print(f"Username: {username} , Password:{password}")
             print("You can now procced to log in to your account")
 
-#         elif number == "2":
-#             '''
-#             Logs in the user in to their already created password locker account    
-#             '''
-#             print("Provide username...")
-#             username = input()
+        elif number == "2":
+            '''
+            Logs in the user in to their already created password locker account    
+            '''
+            print("Provide username...")
+            username = input()
 
-#             print("Provide the password...")
-#             password = input()
+            print("Provide the password...")
+            password = input()
 
-#             print("Use the following choices to navigate through credentials...")
+            print("Use the following choices to navigate through credentials...")
             
-#             while True :
-#                 '''
-#                 While loop to use after loggin in
-#                 '''
-#                 print('''options:
-#                     1-Create a new credential - your own password
-#                     2-Create a new credential - generated password
-#                     3-Display existing account credentials
-#                     ''')
-#                 number = input()
-
-
-#                 if number == "1":
-#                     '''
-#                     creating a new credential using own password
-#                     '''
-#                     print("Enter the name of the account:")
-#                     account = input()
-#                     print("Enter the username you want for the account:")
-#                     cred_username = input()
-#                     print("Enter the password you desire:")
-#                     cred_password = input()
-
-#                     #save_credentials( create_credentials(self,account,cred_username,cred_password))
-#                     #print(f"Your {account} credentials have been successfully created!")
-
-#                 elif number =="2" : 
-#                     '''
-#                     creating a new credential using generated password
-#                     ''' 
-#                     print("Enter the name of your account:")
-#                     account = input() 
+            while True :
+                '''
+                While loop to use after loggin in
+                '''
+                print('''options:
+                    1-Create a new credential - your own password
+                    2-Create a new credential - generated password
+                    3-Display existing account credentials
+                    ''')
+#                
 #                     print("Enter the username you want for the account:") 
 #                     cred_username = input() 
 #                     save_credentials(Credential(account, cred_username, (generating_password())))  
