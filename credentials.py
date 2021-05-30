0966f531b676082aede83dd1c60991f7ebeeb5fa
Tajeu2001 <tajeusanta@gmail.com>
2021-05-31 00:49:01 +0300

    Add a function that deletes a credential

--- credentials.py
+++ credentials.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 '''
 Credentials class for creating user credentials
 '''
@@ -10,14 +14,14 @@ class Credential:
 
     credential_list = []
 
-    def __init__(self,account,username,password):
+    def __init__(self,account,cred_username,cred_password):
       '''
       method to define properties for the credential class object
       '''
       
       self.account = account
-      self.username = username
-      self.password = password
+      self.cred_username = cred_username
+      self.cred_password = cred_password
 
     def save_credential(self):
       '''
@@ -25,3 +29,30 @@ class Credential:
       '''
 
       Credential.credential_list.append(self)
+
+    def delete_credential(self):
+      '''
+      Method for deleting credentials
+      '''
+      Credential.credential_list.remove(self)  
+
+    # @classmethod
+    # def generate_password(cls):
+    #   '''
+    #   Method that generates a random password
+    #   '''
+    #   size = 6
+    #   rand_password = string.ascii_uppercase + string.ascii_lowercase
+    #   password = ''.join(random.choice(rand_password) for num in range(size))  
+
+    #   return password
+    #   print(password)
+
+
+    # @classmethod
+    # def view_credentials(cls):  
+    #   '''
+    #   method that allows users to view saved credentials in credentials list
+    #   '''
+    #   return cls.credential_list    
+  
